22f9d969170d193ea62fb77d9b1faa0801f2dfed
Peti <peter.hathazi99@gmail.com>
2022-10-20 11:19:21 +0200

    Update App.js

--- asknasa/src/App.js
+++ asknasa/src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import './App.css';
 import NasaContext from "./context.js";
 import DayPickerComponent from "./components/dayPickerComponent.js";
+import PictureOfTheDay from './components/pictureContainer.js';
 
 function App() {
   const [date, setDate] = useState(new Date().toString());
@@ -15,6 +16,9 @@ function App() {
               Nasa took a picture of our universe each day! Select a date, and check out that day's picture!
             </p>
             <DayPickerComponent />
+          </div>
+          <div>
+            <PictureOfTheDay />
           </div>
         </div>
       </NasaContext.Provider>
